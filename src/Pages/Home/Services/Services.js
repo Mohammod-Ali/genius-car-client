4446a91569b8a65f7d185a463c4505905313b5ff
Mohammod Ali <mohammodali651@gmail.com>
2022-11-13 01:12:45 +0600

    search btn added

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,14 +1,22 @@
 import React, { useEffect, useState } from 'react';
+import { useRef } from 'react';
 import ServiceCart from './ServiceCart';
 
 const Services = () => {
     const [services, setServices] = useState([])
+    const [isAsc, setIsAsc] = useState(true)
+    const [search, setSearch] = useState('')
+    const searchRef = useRef()
 
     useEffect( () => {
-        fetch('http://localhost:5000/services')
+        fetch(`http://localhost:5000/services?search=${search}&order=${isAsc ? 'asc' : 'desc'}`)
         .then(res => res.json())
         .then(data => setServices(data))
-    },[])
+    },[isAsc])
+
+    const handleSearch = () =>{
+        setSearch(searchRef.current.value)
+    }
 
     return (
         <div>
@@ -16,6 +24,8 @@ const Services = () => {
                 <p className='text-2xl font-bold text-orange-600'>Service</p>
                 <h2 className='text-5xl font-semibold'>Our Service Area</h2>
                 <p>the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+                <input ref={searchRef} type="text" name="" id="" /> <button  onClick={handleSearch}>Search</button>
+                <button className='btn' onClick={() => setIsAsc(!isAsc)}>{isAsc ? 'desc' : 'asc'}</button>
             </div>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
                 {
